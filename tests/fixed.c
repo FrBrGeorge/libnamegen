d6fa21c90d9e626e07b2345c3a132247032b9a17
Your Name <you@example.com>
2020-11-01 18:07:02 +0000

    Other test now also dummy

---
+++ tests/fixed.c
@@ -0,0 +1,17 @@
+#include <check.h>
+#include "namegen.h"
+
+#define SZ 40
+char name[SZ];
+
+START_TEST(namegen) {
+	unsigned long seed = 100500;
+	namegen(name, sizeof(name), "!sV'i", &seed);
+	ck_assert_str_eq(name, "QQevo");
+}
+
+int main(void)
+{
+    return 0;
+}
+
