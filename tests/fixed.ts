ea4d3d382d22f04d73ced0d20f3aa0c8cd76ea6c
Your Name <you@example.com>
2020-11-01 18:38:47 +0000

    Use checkmk

--- tests/fixed.ts
+++ tests/fixed.ts
@@ -1,17 +1,9 @@
-#include <check.h>
 #include "namegen.h"
 
 #define SZ 40
 char name[SZ];
 
-START_TEST(tnamegen) {
+#test tnamegen
 	unsigned long seed = 100500;
 	namegen(name, sizeof(name), "!sV'i", &seed);
-	ck_assert_str_eq(name, "QQevo");
-}
-
-int main(void)
-{
-    return 0;
-}
-
+	ck_assert_str_eq(name, "Aleei'bum");
