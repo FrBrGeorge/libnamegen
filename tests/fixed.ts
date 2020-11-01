5c3e543c58fe6eab8f7b0503e1bfa9f857d9f677
Your Name <you@example.com>
2020-11-01 19:51:10 +0000

    Another ts

--- tests/fixed.ts
+++ tests/fixed.ts
@@ -2,8 +2,13 @@
 
 #define SZ 40
 char name[SZ];
+unsigned long seed = 100500;
 
+#suite Fixed
 #test tnamegen
-	unsigned long seed = 100500;
 	namegen(name, sizeof(name), "!sV'i", &seed);
 	ck_assert_str_eq(name, "Aleei'bum");
+
+#test tnamegen2
+	namegen(name, sizeof(name), "!<B|D>Vs (from|of) !<B|D>Vs", &seed);
+	ck_assert_str_eq(name, "Featur from Dauech");
