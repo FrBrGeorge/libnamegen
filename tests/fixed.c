55d80441b6ad251a0891a55e459ed4cbd04b87f4
Your Name <you@example.com>
2020-11-01 18:23:52 +0000

    Reaname test

--- tests/fixed.c
+++ tests/fixed.c
@@ -4,7 +4,7 @@
 #define SZ 40
 char name[SZ];
 
-START_TEST(namegen) {
+START_TEST(tnamegen) {
 	unsigned long seed = 100500;
 	namegen(name, sizeof(name), "!sV'i", &seed);
 	ck_assert_str_eq(name, "QQevo");
